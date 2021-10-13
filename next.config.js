47828c9360ca30ed5b4a3010607da6f1d7697cd9
Jean Humblot <jeanhumblot.dev@gmail.com>
2021-10-14 00:24:27 +0900

    Deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    loader: 'imgix',
+    path: 'https://noop/',
+  },
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
